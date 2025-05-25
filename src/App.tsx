c3c521f668c668cd99dea123c2f7cae7f02f34b3
Zachary Campanelli <zackjcampanelli@gmail.com>
2025-05-24 23:36:58 -0400

    feat: update HTML structure and styles for Pomodoro app and addition of basic CSS Styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 function App() {
   return (
     <ChakraProvider value={defaultSystem}>
-      <div></div>
+      <div>
+        <h1>pomodoro</h1>
+      </div>
     </ChakraProvider>
   );
 }
